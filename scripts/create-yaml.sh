756cc10cf2742bd3564c04f3e3543581912d21c0
Chechu <chechu@ChechusLaptop.local>
2022-11-15 19:55:52 +0100

    first commit

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
 MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
-CHART_DIR=$(cd "${MODULE_DIR}/charts/ibm-mq-operator"; pwd -P)
+CHART_DIR=$(cd "${MODULE_DIR}/charts/ibm-waiops-operator"; pwd -P)
 
 NAME="$1"
 DEST_DIR="$2"
