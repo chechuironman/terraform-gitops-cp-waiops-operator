7ea5b139793e2226ad85e8980cb7964ae870e5ad
Chechu <chechu@ChechusLaptop.local>
2022-11-16 14:15:40 +0100

    added ocp-virt code to provision vm

--- test/stages/stage1-dependency-management.tf
+++
@@ -1,3 +0,0 @@
-module "cp4i-dependencies" {
-  source = "github.com/cloud-native-toolkit/terraform-cp4i-dependency-management"
-}
--- test/stages/stage2-gitops-cp-waiops.tf
+++
@@ -1,8 +0,0 @@
-module gitops_module {
-  source = "./module"
-
-  gitops_config = module.gitops.gitops_config
-  git_credentials = module.gitops.git_credentials
-  server_name = module.gitops.server_name
-  catalog = module.cp_catalogs.catalog_ibmoperators
-}
